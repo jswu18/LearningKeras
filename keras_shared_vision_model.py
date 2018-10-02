7a736f1ea0ca354e7f26e3d4e1443518752be877
jswu18 <js.wu@mail.utoronto.ca>
2018-10-02 14:19:28 -0400

    added TODO for a second data set for multiple output training

--- keras_shared_vision_model.py
+++ keras_shared_vision_model.py
@@ -3,7 +3,6 @@ import keras
 from keras.layers import Conv2D, MaxPooling2D, Input, Dense, Flatten
 from keras.models import Model
 from keras.datasets import mnist
-import matplotlib.pyplot as plt
 
 class SharedVisionModel:
     '''
@@ -56,6 +55,8 @@ class SharedVisionModel:
             idx_false = idx_false[0:half_desired_size]
             idx_true = idx_true[0:half_desired_size]
             data['labels'] = np.concatenate((labels[idx_true], labels[idx_false]))
+
+            #TODO create dataset of one hot encoded digits to train the visual model with the classification mdoel
             data['digit_a'] = np.concatenate((digit_a[idx_true], digit_a[idx_false]))
             data['digit_b'] = np.concatenate((digit_b[idx_true], digit_b[idx_false]))
             return data
